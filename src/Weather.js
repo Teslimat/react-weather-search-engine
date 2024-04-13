ae06c5430882d06838bf6ee4f08aa637e6d5cc6c
Teslimat <teslimatadeyemi@gmail.com>
2024-04-13 09:49:50 +0100

    imported Axios into the application

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css"
+import axios from "axios"
 
-export default function Weather() {
+
+export default function Weather(props) {
+    let [weatherData, setWeatherData] = useState({ready: false})
+    let [city, setCity] = useState(props.defaultCity)
+
+    function handleresponse(response){
+        console.log(response.data)
+        setWeatherData({
+            ready: true,
+            city: response.data.name,
+            description: response.data.weather[0].description,
+            tempperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed
+        })
+    }
+
+    function search(){
+    let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleresponse)
+    }
+
+
+    function handleSubmit(event){
+        event.preventDefault();
+        search(city);
+    }
+
+    function changeCity(event){
+        setCity(event.target.value)
+    }
+
+    if(weatherData.ready){
   return (
     <div className="Weather">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
             <input
@@ -12,6 +46,7 @@ export default function Weather() {
               placeholder="Enter a city"
               autoFocus="on"
               className="form-control"
+              onChange={changeCity}
             />
           </div>
           <div className="col-3">
@@ -21,33 +56,37 @@ export default function Weather() {
               className="btn btn-primary w-100"
             />
           </div>
-          <h1>Paris</h1>
-          <ul>
-            <li>Sunday 13:31</li>
-            <li>Clear Sky</li>
-          </ul>
-          <div className="row mt-3">
-            <div className="col-6">
-              <div className="clearfix">
-                <img
-                  src="https://s.yimg.com/os/weather/1.0.1/shadow_icon/60x60/partly_cloudy_day@2x.png"
-                  alt="Clear Sky"
-                />
-                <span className="WeatherTemperature">
-                  <span className="temperature"> 15 </span>
-                  <span className="unit">°C</span>
-                </span>
-              </div>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Humidity: 44%</li>
-                <li>Wind: 6.17Km/h</li>
-              </ul>
-            </div>
-          </div>
         </div>
       </form>
+      <h1>{weatherData.city}</h1>
+      <ul>
+        <li>Sunday 13:31</li>
+        <li className="text-capitalize">{weatherData.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://s.yimg.com/os/weather/1.0.1/shadow_icon/60x60/partly_cloudy_day@2x.png"
+              alt="Clear Sky"
+            />
+            <span className="WeatherTemperature">
+              <span className="temperature"> {Math.round(weatherData.tempperature)} </span>
+              <span className="unit">°C</span>
+            </span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}Km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
+    } else {
+        search();
+        return "Loading...";
+    }
 }
